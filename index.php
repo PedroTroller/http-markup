f2c281934a2a2111b01b9048464ca36938573ae4
Pierre PLAZANET <plazanet.pierre@gmail.com>
2020-11-04 14:49:59 +0100

    feat: return 415 response

--- index.php
+++ index.php
@@ -5,112 +5,125 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Symfony\Component\Process\Process;
+use Slim\App;
+use React\Promise\Deferred;
+use React\Promise\Promise;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$getExtensionFromRequest = function (Request $request): string {
-    $type = $request->getHeader('Content-Type');
+$app = new App();
 
-    if (empty($type)) {
-        throw new Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException(
-            'No Content-Type provided'
-        );
-    }
-
-    if (is_array($type)) {
-        $type = current($type);
-    }
-
-    $formats = [
-        'text/asciidoc'         => 'asc',
-        'text/creole'           => 'creole',
-        'text/markdown'         => 'md',
-        'text/org'              => 'org',
-        'text/orgmode'          => 'org',
-        'text/rdoc'             => 'rdoc',
-        'text/restructuredtext' => 'rst',
-        'text/rst'              => 'rst',
-        'text/textile'          => 'textile',
-        'text/txstyle'          => 'textile',
-        'text/wiki'             => 'wiki',
+$app->post('/', function (Request $request, Response $response, array $args): Response {
+    $result = [
+        'status' => 502,
+        'body' => '',
     ];
 
-    if (false === array_key_exists($type, $formats)) {
-        throw new Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException(
-            sprintf('Unsupported Content-Type. Only %s supported', implode(', ', array_keys($formats)))
-        );
-    }
+    $deferred = new Deferred();
+    $promise = $deferred->promise();
 
-    return $formats[$type];
-};
+    $promise
+        ->then(function (Request $request): array {
+            $type = $request->getHeader('Content-Type');
 
-$markupToHtml = function (string $markup, string $extension): string {
-    $temporaryFile = sprintf('%s/%s.%s', sys_get_temp_dir(), uniqid(), $extension);
-    $process = new Process(
-        [
-            'github-markup',
-            $temporaryFile,
-        ]
-    );
+            if (empty($type)) {
+                throw new Exception('No Content-Type provided', 415);
+            }
 
-    file_put_contents($temporaryFile, $markup);
-    $process->run();
-    unlink($temporaryFile);
+            if (is_array($type)) {
+                $type = current($type);
+            }
 
-    if ($process->isSuccessful()) {
-        return $process->getOutput();
-    }
+            $formats = [
+                'text/asciidoc'         => 'asc',
+                'text/creole'           => 'creole',
+                'text/markdown'         => 'md',
+                'text/org'              => 'org',
+                'text/orgmode'          => 'org',
+                'text/rdoc'             => 'rdoc',
+                'text/restructuredtext' => 'rst',
+                'text/rst'              => 'rst',
+                'text/textile'          => 'textile',
+                'text/txstyle'          => 'textile',
+                'text/wiki'             => 'wiki',
+            ];
 
-    throw new Exception($process->getErrorOutput());
-};
+            if (false === array_key_exists($type, $formats)) {
+                throw new Exception(
+                    sprintf('Unsupported Content-Type. Only %s supported', implode(', ', array_keys($formats))),
+                    415,
+                );
+            }
 
-$prettier = function (string $html): string {
-    $temporaryFile = sprintf('%s/%s.%s', sys_get_temp_dir(), uniqid(), 'html');
-    $process = new Process(
-        [
-            'prettier',
-            '--write',
-            '--ignore-unknown',
-            '--parser',
-            'html',
-            '--tab-width',
-            2,
-            '--print-width',
-            1000,
-            $temporaryFile,
-        ]
-    );
+            return [$formats[$type], (string) $request->getBody()];
+        })
+        ->then(function (array $data): string {
+            [$extension, $markup] = $data;
+            $temporaryFile = sprintf('%s/%s.%s', sys_get_temp_dir(), uniqid(), $extension);
+            $process = new Process(
+                [
+                    'github-markup',
+                    $temporaryFile,
+                ]
+            );
 
-    file_put_contents($temporaryFile, $html);
-    $process->run();
-    $html = (string) file_get_contents($temporaryFile);
-    unlink($temporaryFile);
+            file_put_contents($temporaryFile, $markup);
+            $process->run();
+            unlink($temporaryFile);
 
-    if ($process->isSuccessful()) {
-        return $html;
-    }
+            if ($process->isSuccessful()) {
+                return $process->getOutput();
+            }
 
-    throw new Exception($process->getErrorOutput());
-};
+            throw new Exception($process->getErrorOutput());
+        })
+        ->then(function (string $html): string {
+            $temporaryFile = sprintf('%s/%s.%s', sys_get_temp_dir(), uniqid(), 'html');
+            $process = new Process(
+                [
+                    'prettier',
+                    '--write',
+                    '--ignore-unknown',
+                    '--parser',
+                    'html',
+                    '--tab-width',
+                    2,
+                    '--print-width',
+                    1000,
+                    $temporaryFile,
+                ]
+            );
 
-$app = new \Slim\App();
+            file_put_contents($temporaryFile, $html);
+            $process->run();
+            $html = (string) file_get_contents($temporaryFile);
+            unlink($temporaryFile);
 
-$app->post('/', function (Request $request, Response $response, array $args) use ($getExtensionFromRequest, $markupToHtml, $prettier): void {
-    $extension = $getExtensionFromRequest($request);
-    $markup = (string) $request->getBody();
+            if ($process->isSuccessful()) {
+                return $html;
+            }
 
-    try {
-        $html = $markupToHtml($markup, $extension);
-        $html = $prettier($html);
-        $response->getBody()->write($html);
-        $response->withStatus(200);
-    } catch (Exception $exception) {
-        $response->withStatus(500);
-    }
+            throw new Exception($process->getErrorOutput());
+        })
+        ->otherwise(function (Exception $exception) use (&$result) {
+            $result['status'] = $exception->getCode() ?: 500;
+            $result['body'] = $exception->getMessage();
+        })
+        ->then(function (string $html) use (&$result): void {
+            $result['status'] = 200;
+            $result['body'] = $html;
+        })
+    ;
+
+    $deferred->resolve($request);
+
+    $response->getBody()->write($result['body']);
+
+    return $response->withStatus($result['status']);
 });
 
-$app->get('/_ping', function (Request $request, Response $response, array $args): void {
-  $response->withStatus(200);
+$app->get('/_ping', function (Request $request, Response $response, array $args): Response {
+      return $response->withStatus(200);
 });
 
 $app->run();
